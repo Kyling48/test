7bc0c10b3c07d0850bc9aee6deedc76e7dc99ceb
Kyling48 <130426839+Kyling48@users.noreply.github.com>
2023-04-11 11:34:17 +0800

    Add files via upload

---
+++ World.java
@@ -0,0 +1,8 @@
+public class World{
+	public void main(String[] args){
+		System.out.println("World line 1")
+		System.out.println("World line 2")
+		System.out.println("World line 3")
+		System.out.println("World line 4 edit by master")
+	}
+}
